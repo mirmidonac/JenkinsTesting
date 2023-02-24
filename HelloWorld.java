3044ea50c03da2ed0e79b55b763cd4f6d3ac33ee
mirmidonac <pavlovich.marko@gmail.com>
2023-02-24 16:42:16 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main (String[] args){
 	System.out.println("*******HELLO MARE CARE********");
+	System.out.println("*******WEBHOOK ACTIVATED********");	
 	}
 }
