ea341e4764ea224eab5d66b4e74d675cb78e73b2
mirmidonac <pavlovich.marko@gmail.com>
2023-03-21 17:39:32 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,14 +2,8 @@ public class HelloWorld {
 	public static void main (String[] args){
 	System.out.println("*******HELLO MARE CARE********");
 	System.out.println("*******!!!!WEBHOOK ACTIVATED!!!!********");	
-		Int rand = 4 ; 
-      // Setting the upper bound to generate the
-      // random numbers in specific range
+	int rand = 4 ; 
       int upperbound = 25;
-      // Generating random values from 0 - 24 
-      // using nextInt()
-     
-      // Printing the generated random numbers
       System.out.println(rand);
       System.out.println(upperbound);
  
