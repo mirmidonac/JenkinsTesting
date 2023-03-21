a624b228b611e947012fd57d1a167890aa4e1fe3
mirmidonac <pavlovich.marko@gmail.com>
2023-03-21 17:30:56 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,24 @@
+
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main (String[] args){
 	System.out.println("*******HELLO MARE CARE********");
 	System.out.println("*******!!!!WEBHOOK ACTIVATED!!!!********");	
+		
 	}
+}
+
+public class Echo
+{
+  public static void main (String[] args) 
+  {
+    String inData;
+    Scanner scan = new Scanner( System.in );
+
+    System.out.println("Enter the data:");
+    inData = scan.nextLine();
+
+    System.out.println("You entered:" + inData );
+  }
 }
