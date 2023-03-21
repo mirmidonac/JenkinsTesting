42e3ab59157aaa4f47f122c3bde8bcbca70b6d27
mirmidonac <pavlovich.marko@gmail.com>
2023-03-21 17:34:07 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,4 @@
-
-import java.util.Scanner;
+import java.util.Random;
 
 public class HelloWorld {
 	public static void main (String[] args){
@@ -9,16 +8,26 @@ public class HelloWorld {
 	}
 }
 
-public class Echo
-{
-  public static void main (String[] args) 
-  {
-    String inData;
-    Scanner scan = new Scanner( System.in );
-
-    System.out.println("Enter the data:");
-    inData = scan.nextLine();
-
-    System.out.println("You entered:" + inData );
-  }
+class GenerateRandom {
+    public static void main( String args[] ) {
+      // Instance of random class
+      Random rand = new Random(); 
+      // Setting the upper bound to generate the
+      // random numbers in specific range
+      int upperbound = 25;
+      // Generating random values from 0 - 24 
+      // using nextInt()
+      int int_random = rand.nextInt(upperbound); 
+      // Generating random using nextDouble 
+      // in 0.0 and 1.0 range
+      double double_random = rand.nextDouble();
+      // Generating random using nextDouble
+      // in 0.0 and 1.0 range
+      float float_random = rand.nextFloat();
+      
+      // Printing the generated random numbers
+      System.out.println("Random integer value from 0 to" + (upperbound-1) + " : " + int_random);
+      System.out.println("Random float value between 0.0 and 1.0 : " + float_random);
+      System.out.println("Random double value between 0.0 and 1.0 : " + double_random);
+    }
 }
