b3399a61f32a76b4201c51cd0998335025fab7b2
mirmidonac <pavlovich.marko@gmail.com>
2023-03-22 14:49:36 +0100

    Adding Java random utili

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,14 @@
+import java.util.Random;
+
 public class HelloWorld {
 	public static void main (String[] args){
+	Random rand = new Random(); 
 	System.out.println("*******HELLO MARE CARE********");
 	System.out.println("*******!!!!WEBHOOK ACTIVATED!!!!********");	
 	int rand = 4 ; 
       int upperbound = 25;
       System.out.println(rand);
       System.out.println(upperbound);
- 
-		
+     System.out.println(rand);
 	}
 }
